95aead31ea06cdc3a295691425ba29ccf71dc1a6
Nathan <85586026+nathlinear@users.noreply.github.com>
2026-02-25 02:11:43 -0800

    Add label for data

--- button_controller.gd
+++ button_controller.gd
@@ -5,7 +5,7 @@ extends Node
 @export var pass_field: LineEdit
 @export var status: Label
 @export var get_quiz_data_button: Button
-@export var quiz_data_label: Label
+@export var quiz_data_label: RichTextLabel
 
 func _ready() -> void:
 	button1.pressed.connect(sign_in)
@@ -21,7 +21,12 @@ func _on_signed_in(user : SupabaseUser):
 
 func get_data() -> void:
 	Supabase.database.Rpc("get_my_quiz_answers")
+	
+	if Supabase.database.rpc_completed.has_connections():
+		return
+	
 	Supabase.database.rpc_completed.connect(result)
 
 func result(query_result) -> void:
 	print(query_result)
+	quiz_data_label.text = str(query_result)

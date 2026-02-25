9c82a583be7ff54357d34a912934808b9b3a1cdf
Nathan <85586026+nathlinear@users.noreply.github.com>
2026-02-25 02:00:31 -0800

    Add reading quiz answers

--- button_controller.gd
+++ button_controller.gd
@@ -4,14 +4,12 @@ extends Node
 @export var email_field: LineEdit
 @export var pass_field: LineEdit
 @export var status: Label
+@export var get_quiz_data_button: Button
+@export var quiz_data_label: Label
 
 func _ready() -> void:
-	if button1 == null: print("Button 1 not specified in controller!")
-	if email_field == null: print("Email not specified in controller!")
-	if pass_field == null: print("Pass not specified in controller!")
-	if status == null: print("Status not specified in controller!")
-	
 	button1.pressed.connect(sign_in)
+	get_quiz_data_button.pressed.connect(get_data)
 
 func sign_in():
 	Supabase.auth.signed_in.connect(_on_signed_in)
@@ -20,3 +18,10 @@ func sign_in():
 func _on_signed_in(user : SupabaseUser):
 	print("Successfully signed as ", user)
 	status.text = str(user)
+
+func get_data() -> void:
+	Supabase.database.Rpc("get_my_quiz_answers")
+	Supabase.database.rpc_completed.connect(result)
+
+func result(query_result) -> void:
+	print(query_result)

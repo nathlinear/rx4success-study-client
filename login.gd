add890a3776fea8fbbb6b15b0a590b84b4f30ef8
Nathan <85586026+nathlinear@users.noreply.github.com>
2026-03-21 12:28:43 -0700

    Add username creation UI

--- login.gd
+++ login.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var pass_field: LineEdit
 @export var login_button: Button
 @export var info: RichTextLabel
+@export var passwordVBox: VBoxContainer
+@export var usernameVBox: VBoxContainer
 
 func _ready() -> void:
 	# connect buttons
@@ -12,14 +14,43 @@ func _ready() -> void:
 	# connect signals
 	Supabase.auth.signed_in.connect(_on_signed_in)
 	Supabase.auth.error.connect(_error)
+	
+	passwordVBox.visible = true
+	usernameVBox.visible = false
 
 func sign_in():
 	Supabase.auth.sign_in(email_field.text, pass_field.text)
 
-func _on_signed_in(user : SupabaseUser):
-	print("Successfully signed as ", user)
-	get_tree().change_scene_to_file("res://mainMenu.tscn")
-
 func _error(e: SupabaseAuthError):
 	print(e.message)
 	info.text = e.message
+
+func _on_signed_in(user : SupabaseUser):
+	print("Successfully signed as ", user)
+	if has_username():
+		_load_menu()
+	
+	# is true when user confirms new username creation
+	var success = make_username() 
+	
+	if success:
+		_load_menu()
+	else:
+		# user did not want to create username or errored
+		# sign out and return to initial page
+		passwordVBox.visible = true
+		usernameVBox.visible = false
+		Supabase.auth.sign_out()
+
+func _load_menu() -> void:
+	get_tree().change_scene_to_file("res://mainMenu.tscn")
+
+func has_username() -> bool:
+	# TODO
+	return true
+
+func make_username() -> bool:
+	# TODO
+	usernameVBox.visible = true
+	passwordVBox.visible = false
+	return true

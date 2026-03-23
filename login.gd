4d882dc99b809096ba81b736b767be6fcdea4930
Nathan <85586026+nathlinear@users.noreply.github.com>
2026-03-23 11:06:44 -0700

    Add sign up and usernames

--- login.gd
+++ login.gd
@@ -4,38 +4,39 @@ extends Node2D
 @export var pass_field: LineEdit
 @export var login_button: Button
 @export var info: RichTextLabel
+@export var register_button: Button
 @export var passwordVBox: VBoxContainer
 @export var usernameVBox: VBoxContainer
+@export var createUsernameButton: Button
+@export var createUsernameField: LineEdit
 
 func _ready() -> void:
 	# connect buttons
 	login_button.pressed.connect(sign_in)
+	register_button.pressed.connect(sign_up)
 	
 	# connect signals
 	Supabase.auth.signed_in.connect(_on_signed_in)
-	Supabase.auth.error.connect(_error)
+	Supabase.auth.error.connect(_auth_error)
+	Supabase.database.inserted.connect(_on_inserted)
+	Supabase.database.selected.connect(_on_selected)
+	Supabase.database.error.connect(_db_error)
 	
 	passwordVBox.visible = true
 	usernameVBox.visible = false
 
-func sign_in():
-	Supabase.auth.sign_in(email_field.text, pass_field.text)
-
-func _error(e: SupabaseAuthError):
-	print(e.message)
-	info.text = e.message
-
 func _on_signed_in(user : SupabaseUser):
 	print("Successfully signed as ", user)
-	if has_username():
-		_load_menu()
+	var username = await get_username()
+	if username != "":
+		load_menu()
 		return
 	
 	# is true when user confirms new username creation
-	var success = make_username() 
+	var success = await make_username() 
 	
 	if success:
-		_load_menu()
+		load_menu()
 		return
 	else:
 		# user did not want to create username or errored
@@ -44,15 +45,93 @@ func _on_signed_in(user : SupabaseUser):
 		usernameVBox.visible = false
 		Supabase.auth.sign_out()
 
-func _load_menu() -> void:
+func _on_signed_out() -> void:
+	print("Signed out")
+
+func _on_inserted(result: Array) -> void:
+	print(result)
+
+func _on_selected(result: Array) -> void:
+	print(result)
+	if Supabase.auth.client == null:
+		print("Not logged in!")
+	print(Supabase.auth.client)
+
+func _db_error(e: SupabaseDatabaseError) -> void:
+	#print(e.message)
+	if e.message.begins_with("duplicate"):
+		info.text = "Username already taken."
+	else:
+		info.text = e.message
+
+func _auth_error(e: SupabaseAuthError):
+	print(e.message)
+	info.text = e.message
+
+func load_menu() -> void:
 	get_tree().change_scene_to_file("res://mainMenu.tscn")
+
+func sign_in():
+	var task: AuthTask = Supabase.auth.sign_in(email_field.text, pass_field.text)
+	await task.completed
+	if task.error:
+		print(task.error.message)
+		info.text = task.error.message
+
+func sign_up():
+	var task: AuthTask = Supabase.auth.sign_up(email_field.text, pass_field.text)
+	await task.completed
+	if task.error:
+		print(task.error.message)
+		info.text = task.error.message
+	else:
+		sign_in()
 
 func has_username() -> bool:
 	# TODO
 	return true
 
 func make_username() -> bool:
-	# TODO
 	usernameVBox.visible = true
 	passwordVBox.visible = false
+	while true:
+		await createUsernameButton.pressed
+		var username: String = createUsernameField.text
+		if username.length() < 1:
+			info.text = "Username cannot be blank."
+			print("username cannot be blank. try again")
+			continue
+		await insert_username(createUsernameField.text)
+		var res = await get_username()
+		print(res == "")
+		if res != "":
+			break
 	return true
+
+func get_username() -> String:
+	var query = SupabaseQuery.new().from("user_profiles").select(["username"])
+	var task: DatabaseTask = Supabase.database.query(query)
+	await task.completed
+	if task.data != null and not task.data.is_empty():
+		return str(task.data[0].username)
+	return ""
+
+
+func login() -> void:
+	Supabase.auth.sign_in("a@u.pacific.edu", "your-password")
+
+
+func logout() -> void:
+	Supabase.auth.sign_out()
+
+func insert_username(username: String) -> void:
+	var query = (
+		SupabaseQuery.new()
+		.from("user_profiles")
+		.insert(
+			[{"username": username}]
+		)
+	)
+	var task = Supabase.database.query(query)
+	await task.completed
+	
--- main_menu.gd
+++ main_menu.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 	
 	Supabase.database.rpc_completed.connect(_response)
 	Supabase.database.error.connect(_error)
+	show_username()
 
 func _go_to_quiz():
 	get_tree().change_scene_to_file("res://quiz.tscn")
@@ -21,7 +22,8 @@ func _get_question():
 	Supabase.database.Rpc("get_question")
 
 func _logout():
-	Supabase.auth.sign_out()
+	var task: AuthTask = Supabase.auth.sign_out()
+	await task.completed
 	get_tree().change_scene_to_file("res://login.tscn")
 
 func _response(msg: Dictionary):
@@ -30,3 +32,18 @@ func _response(msg: Dictionary):
 
 func _error(e):
 	print(e)
+
+func show_username() -> void:
+	var label = $CanvasLayer/Control/MarginContainer/VBoxContainer/Label
+	label.text = "Logged in as: %s" % await get_username()
+
+func get_username() -> String:
+	if Supabase.auth.client == null:
+		return "Not logged in"
+	
+	var query = SupabaseQuery.new().from("user_profiles").select(["username"])
+	var task: DatabaseTask = Supabase.database.query(query)
+	await task.completed
+	if task.data != null and not task.data.is_empty():
+		return str(task.data[0].username)
+	return ""

5f89398c5466b319a02e9a7800617d4a8be02f56
James I. Uriarte <j_uriarte@u.pacific.edu>
2026-04-14 21:06:27 -0700

    quiz hud fixed

--- custom_quiz_setup.gd
+++ custom_quiz_setup.gd
@@ -19,12 +19,31 @@ func _ready() -> void:
 	questions_spinbox.step = 1
 	questions_spinbox.value = CustomQuizSettings.question_count
 
+	var time_edit := time_spinbox.get_line_edit()
+	var questions_edit := questions_spinbox.get_line_edit()
+
+	time_edit.focus_entered.connect(_on_time_focus)
+	questions_edit.focus_entered.connect(_on_questions_focus)
+
 	start_button.pressed.connect(_start_custom_quiz)
 	back_button.pressed.connect(_go_back)
 
+func _on_time_focus() -> void:
+	time_spinbox.get_line_edit().call_deferred("select_all")
+
+func _on_questions_focus() -> void:
+	questions_spinbox.get_line_edit().call_deferred("select_all")
+
 func _start_custom_quiz() -> void:
-	CustomQuizSettings.time_minutes = int(time_spinbox.value)
-	CustomQuizSettings.question_count = int(questions_spinbox.value)
+	var time_val := int(time_spinbox.value)
+	var question_val := int(questions_spinbox.value)
+
+	time_val = clamp(time_val, 1, 20)
+	question_val = clamp(question_val, 10, 50)
+
+	CustomQuizSettings.time_minutes = time_val
+	CustomQuizSettings.question_count = question_val
+
 	get_tree().change_scene_to_file("res://custom_quiz.tscn")
 
 func _go_back() -> void:
